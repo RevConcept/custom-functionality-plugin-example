e4059687bf200ac94135a95337991db6d939f5b1
Chris <Chris@RevelationConcept.com>
2014-03-28 11:26:09 -0700

    Option to remove submenu pages

--- lib/admin-customizations.php
+++ lib/admin-customizations.php
@@ -109,7 +109,7 @@ add_filter( 'menu_order', 'wordimpress_reorder_menu' );
 function wordimpress_remove_menus() {
 
 //  remove_menu_page( 'index.php' );                  //Dashboard
-//	remove_menu_page( 'edit.php' ); 										//Posts
+//  remove_menu_page( 'edit.php' ); 		      //Posts
 //  remove_menu_page( 'upload.php' );                 //Media
 //  remove_menu_page( 'edit.php?post_type=page' );    //Pages
 //  remove_menu_page( 'edit-comments.php' );          //Comments
@@ -118,6 +118,10 @@ function wordimpress_remove_menus() {
 //  remove_menu_page( 'users.php' );                  //Users
 //  remove_menu_page( 'tools.php' );                  //Tools
 //  remove_menu_page( 'options-general.php' );        //Settings
+
+//  Remove sub-menu pages from Appearance menu
+//  remove_submenu_page( 'themes.php', 'customize.php' ); //Customize
+//  remove_submenu_page( 'themes.php', 'widgets.php' );   //Widgets
 
 }
 
